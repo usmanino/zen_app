cade20ef2332688812910a1e2bc64576e14c850c
Factorial <usmaninobello@gmail.com>
2025-05-20 14:14:55 +0100

    add listing

--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -30,18 +30,61 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     });
   }
 
+  List cars = [
+    'Audi',
+    'BMW',
+    'Mercedes',
+    'Volvo',
+    'Honda',
+    'Toyota',
+    'Lexus',
+    'Ford',
+    'Chevrolet',
+    'Dodge',
+    'Nissan',
+    'Suzuki',
+    'Mazda',
+    'Hyundai',
+    'Kia',
+    'Subaru',
+    'Mitsubishi',
+    'Hyundai',
+  ];
+
+  List foods = [
+    'Pizza',
+    'Burger',
+    'Tacos',
+    'Ice Cream',
+    'Hot Dog',
+    'French Fries',
+    'Sushi',
+    'Ramen',
+    'Spaghetti',
+    'Salad',
+    'Pasta',
+    'Sandwich',
+    'Steak',
+    'Meat',
+  ];
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    textBlinking();
+    // textBlinking();
   }
 
-  void playSound() {
-    player.play(AssetSource('audio/orin.mp3'));
-    // await player.play(UrlSource(
-    //     'https://pixabay.com/music/modern-country-country-song-nobody-is-you-334647/'));
-  }
+  // void playSound() {
+  //   // player.play(
+  //   //   AssetSource('audio/orin.mp3'),
+  //   // );
+  //   player.play(UrlSource(
+  //       'https://firebasestorage.googleapis.com/v0/b/atop-c60ad.appspot.com/o/ORIN%2FEDE%2FEDE%20-%20Bata%20a%20ro%20kokoka.mp3?alt=media&token=8ba1f67a-cb65-4a95-95e4-71f8b32fdce9'));
+  // }
+
+  void playSound() => player.play(UrlSource(
+      'https://firebasestorage.googleapis.com/v0/b/atop-c60ad.appspot.com/o/ORIN%2FEDE%2FEDE%20-%20Bata%20a%20ro%20kokoka.mp3?alt=media&token=8ba1f67a-cb65-4a95-95e4-71f8b32fdce9'));
 
   @override
   Widget build(BuildContext context) {
@@ -49,48 +92,102 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       appBar: AppBar(
         automaticallyImplyLeading: false,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Card(
-                elevation: 20,
-                shadowColor: Colors.red,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: SizedBox(
-                  height: 200,
-                  width: 200,
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Text(text),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Card(
+                    elevation: 20,
+                    shadowColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: SizedBox(
+                      height: 200,
+                      width: 200,
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(text),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                const SizedBox(height: 30),
+                CustomButton(
+                  text: 'Get Started',
+                  bgColor: Colors.tealAccent,
+                  textColor: const Color.fromARGB(255, 60, 59, 59),
+                  onPress: () {
+                    playSound();
+                  },
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 500,
+                        child: ListView.builder(
+                          itemCount: cars.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              padding: const EdgeInsets.all(10),
+                              margin: const EdgeInsets.all(2),
+                              color:
+                                  index % 2 == 0 ? Colors.white : Colors.grey,
+                              child: Row(
+                                children: [
+                                  // const Icon(Icons.car_rental),
+                                  Image.asset(
+                                    'assets/cars/audi.jpg',
+                                    height: 50,
+                                    width: 50,
+                                  ),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(cars[index]),
+                                      Text(DateTime.now().toString()),
+                                    ],
+                                  ),
+                                  const Spacer(),
+                                  const Column(
+                                    children: [
+                                      Text('\$200'),
+                                      Text('Free'),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            );
+                            // ListTile(
+                            //   leading: Icon(Icons.car_rental),
+                            //   title: Text(cars[index]),
+                            //   subtitle: Text(DateTime.now().toString()),
+                            //   trailing: Column(
+                            //     children: [
+                            //       Text('\$200'),
+                            //       Text('Free'),
+                            //     ],
+                            //   ),
+                            // );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
             ),
-            const SizedBox(height: 30),
-            CustomButton(
-              text: 'Get Started',
-              bgColor: Colors.tealAccent,
-              textColor: const Color.fromARGB(255, 60, 59, 59),
-              onPress: () {
-                playSound();
-                // ScaffoldMessenger.of(context).showSnackBar(
-                //   const SnackBar(
-                //     // behavior: SnackBarBehavior.fixed,
-                //     // action: SnackBarAction(
-                //     //   label: 'X',
-                //     //   onPressed: () {},
-                //     // ),
-                //     content: Text('Coming Soon...'),
-                //   ),
-                // );
-              },
-            )
-          ],
+          ),
         ),
       ),
     );
